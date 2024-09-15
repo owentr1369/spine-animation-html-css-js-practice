233f046881a1bb001a178de5c80ce5baad7f193e
TranTam <tamvaa1306@gmail.com>
2024-09-15 19:26:38 +0700

    setup vue

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const mainBtn = document.getElementById("mainBtn");
 mainBtn.addEventListener("click", () => {
   alert("You clicked me!");
 });
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("animation"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  name: "demo animation",
+  path: "./assets/Lopte/skeleton.json",
+});
